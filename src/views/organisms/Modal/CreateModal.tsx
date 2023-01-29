8156efefc583185ca97d16365205609107cd4bb0
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-01-29 21:06:35 +0000

    feat: added esc and arial labels to modal

--- src/views/organisms/Modal/CreateModal.tsx
+++ src/views/organisms/Modal/CreateModal.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as ReactDOM from 'react-dom';
 
 import useModal from 'hooks/useModal';
 
 
 import ModalPayment from './ModalPayment/ModalPayment';
+import { getKey } from 'utils/keyCodes';
 
 
 const doc = document.getElementById('root');
@@ -15,13 +16,25 @@ function CreateModal() {
 
     const modalOptions:any = {
         payment: <ModalPayment config={modalData} />,
-        // configm: <ModalConfirm config={modalData} />
     }
+
+    console.log(modalContext)
+    useEffect(() => {
+        const handleKeyDown = (e:any) => {
+            if (getKey('ESC') === e.keyCode) modalContext.close()
+        };
+
+        document.addEventListener('keydown', handleKeyDown);      
+        return document.addEventListener('keydown', handleKeyDown);
+    }, [])
 
     if(!modalContext.isOpen || !doc) return <></>
     return ReactDOM.createPortal(
         <aside 
-            role="dialog" 
+            role="alertdialog" 
+            aria-modal="true"
+            aria-labelledby={`${modalData.option} ${modalData.type}`}
+            aria-describedby={`${modalData.option} ${modalData.type}`}
             className={`
                 fixed top-0 right-0 bottom-0 left-0 z-50 
                 m-auto opacity-0 bg-black/50
@@ -36,7 +49,3 @@ function CreateModal() {
 }
 
 export default CreateModal;
-
-    // transform: scale(0.8);
-    // opacity: 0;
-    // transition: 200ms ease-in-out;
