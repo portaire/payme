95771c663c97cebad18daa9040700362a9acbc79
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-01-31 16:16:57 +0000

    Update SelectItem.tsx

--- src/views/atoms/Select/_components/SelectItem.tsx
+++ src/views/atoms/Select/_components/SelectItem.tsx
@@ -1,5 +1,5 @@
 function SelectItem({ children, item, value,activeItem, className, ...props }:any) {
-    let isActive = item.name.toLowerCase() === activeItem.toLowerCase();
+    let isActive = item && item.name.toLowerCase() === activeItem.toLowerCase();
 
     return (
     <div tabIndex={-1} name="country" id="country" className={`${isActive ? "bg-blue-100" : "hover:bg-[#F0F8FF]"} text-black py-2 w-full cursor-pointer px-4`} {...props} onClick={() => props.onClick(children)}>
