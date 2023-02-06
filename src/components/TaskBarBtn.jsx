70aadb525436c839c9dc7fed1549bb726daba508
Ana Sobrino <anasonu@hotmail.com>
2023-02-06 18:39:35 +0100

    First commit

---
+++ src/components/TaskBarBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pound from '../svg/Pound'
+
+function TaskBarBtn() {
+  return (
+    <div>
+        <button className='task-bar-btn'> <Pound /> Update payment method</button>
+    </div>
+  )
+}
+
+export default TaskBarBtn
